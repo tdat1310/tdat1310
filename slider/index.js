055ba4f2fb305eb22e7fbf77033a2bf807528239
tdat1310 <cutip59@gmail.com>
2023-02-03 00:03:32 +0700

    mới add nè

--- slider/index.js
+++ slider/index.js
@@ -4,15 +4,21 @@ const nextBtn = document.querySelector(".next-btn");
 var a = 0
 nextBtn.addEventListener("click", () => {
     a = a - 922
-    if(a <= -2743){
+    if(a < -1844){
         a = 0;
         sliderImgs.style.transform = "translateX(0)"
-    }else sliderImgs.style.transform = `translateX( ${a}px)`
+    }else{
+        sliderImgs.style.transform = `translateX( ${a}px)`       
+    }
+    sliderImgs.style.transition= "1s"
 })
 prevBtn.addEventListener("click", () => {   
         a = a + 922
      if(a > 0){
-        a = 0;
-         sliderImgs.style.transform = "translateX(0)"
-     } else sliderImgs.style.transform = `translateX( ${a}px)`    
+        a = -1844;
+         sliderImgs.style.transform = "translateX(-1844px)"    
+     } else {
+        sliderImgs.style.transform = `translateX( ${a}px)`     
+    } 
+     sliderImgs.style.transition= "1s"
 })
